5f80bf96c1356c249810d3f63215cebde3daac94
rogeriolino <>
2012-12-03 16:04:49 -0200

    iniciado modulo atendimento

--- web/modules/sga/atendimento/js/script.js
+++ web/modules/sga/atendimento/js/script.js
@@ -3,4 +3,21 @@ var SGA = SGA || {};
 
 SGA.Atendimento = {
     
+    updateGuiche: function(btnLabel) {
+        var btns = {};
+        btns[btnLabel] = function() {
+            var numero = parseInt($('#numero_guiche').val().trim());
+            if (!isNaN(numero) && numero > 0) {
+                $('#guiche_form').submit();
+            } else {
+                $('#numero_guiche').val('');
+            }
+        }
+        $('#dialog-guiche').dialog({
+            modal: true,
+            width: 200,
+            buttons: btns
+        });
+    }
+    
 }
