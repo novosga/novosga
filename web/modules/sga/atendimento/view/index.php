3b99ee1731046a246c305fa5fc2513c877ec7181
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2012-12-05 17:29:56 -0200

    atualizado módulo de atendimento

--- web/modules/sga/atendimento/view/index.php
+++ web/modules/sga/atendimento/view/index.php
@@ -1,7 +1,32 @@
 <?php
 use \core\SGA;
 
-$guiche = $context->getSession()->get('guiche');
+
+function atendimentoInfo($atendimento) {
+    ?>
+    <ul id="senha_info">
+        <li class="numero">
+            <?php echo _('Senha') ?>
+            <span><?php echo ($atendimento) ? $atendimento->getSenha()->toString() : '' ?></span>
+        </li>
+        <li class="prioridade">
+            <?php echo _('Prioridade') ?>
+            <span><?php echo ($atendimento) ? $atendimento->getSenha()->getPrioridade()->getNome() : '' ?></span>
+        </li>
+        <li class="nome">
+            <?php echo _('Nome') ?>
+            <span><?php echo ($atendimento) ? $atendimento->getCliente()->getNome() : '' ?></span>
+        </li>
+        <li class="servico">
+            <?php echo _('Serviço') ?>
+            <span><?php echo ($atendimento) ? $atendimento->getServicoUnidade()->getNome() : '' ?></span>
+        </li>
+    </ul>
+    <?php
+}
+
+$guiche = $context->getUser()->getGuiche();
+
 ?>
 <div id="dialog-guiche" title="<?php echo _('Guichê') ?>" style="display:none">
     <form id="guiche_form" action="<?php echo SGA::url('set_guiche') ?>" method="post">
@@ -39,13 +64,15 @@ else {
             <a href="javascript:void(0)" onclick="SGA.Atendimento.updateGuiche('<?php echo _('Salvar') ?>')"><?php echo _('Alterar') ?></a>
         </div>
         <div id="controls">
-            <div id="chamar" class="control">
+            <div id="chamar" class="control" style="display:none">
                 <a href="javascript:void(0)" onclick="SGA.Atendimento.chamar()">chamar proximo</a>
             </div>
             <div id="iniciar" class="control" style="display:none">
+                <?php atendimentoInfo($atendimento); ?>
                 <a href="javascript:void(0)" onclick="SGA.Atendimento.iniciar()">iniciar atendimento</a>
             </div>
             <div id="encerrar" class="control" style="display:none">
+                <?php atendimentoInfo($atendimento); ?>
                 <a href="javascript:void(0)" onclick="SGA.Atendimento.encerrar()">encerrar atendimento</a>
             </div>
         </div>
@@ -55,7 +82,10 @@ else {
         </div>
     </div>
     <script type="text/javascript">
-        SGA.Atendimento.init();
+        <?php
+            $status = ($atendimento) ? $atendimento->getStatus() : 1;
+        ?>
+        SGA.Atendimento.init(<?php echo $status ?>);
     </script>
     <?php
 }
