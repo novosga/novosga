8a38c97001b040a9f8a164d52aab71310cf6d008
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2013-01-10 08:32:03 -0200

    Implementada funcionalidade para alterar senhas dos usuários.

--- web/modules/sga/usuarios/UsuariosController.php
+++ web/modules/sga/usuarios/UsuariosController.php
@@ -97,6 +97,23 @@ class UsuariosController extends CrudController {
         $context->getResponse()->jsonResponse($response);
     }
     
+    /**
+     * Verifica se a senha informada é válida e a retorna encriptada.
+     * @param type $senha
+     * @param type $confirmacao
+     * @return type
+     * @throws Exception
+     */
+    private function verificaSenha($senha, $confirmacao) {
+        if (strlen($senha) < 6) {
+            throw new Exception(_('A senha deve possuir no mínimo 6 caracteres.'));
+        }
+        if ($senha != $confirmacao) {
+            throw new Exception(_('A confirmação de senha não confere com a senha.'));
+        }
+        return Security::passEncode($senha);
+    }
+    
     protected function preSave(SGAContext $context, SequencialModel $model) {
         if ($model->getId() == 0) {
             // para novos usuarios, tem que informar a senha
@@ -108,15 +125,10 @@ class UsuariosController extends CrudController {
                 throw new Exception(_('O login deve possuir entre 5 e 20 caracteres (letras ou números).'));
             }
             $senha = Arrays::value($_POST, 'senha');
-            $senha2 = Arrays::value($_POST, 'senha2');
-            if (strlen($senha) < 6) {
-                throw new Exception(_('A senha deve possuir no mínimo 6 caracteres.'));
-            }
-            if ($senha != $senha2) {
-                throw new Exception(_('A confirmação de senha não confere com a senha.'));
-            }
+            $confirmacao = Arrays::value($_POST, 'senha2');
+            
+            $model->setSenha($this->verificaSenha($senha, $confirmacao));
             $model->setStatus(1);
-            $model->setSenha(Security::passEncode($senha));
             $model->setSessionId('');
         } else {
             $model->setStatus((int) Arrays::value($_POST, 'status'));
@@ -169,6 +181,28 @@ class UsuariosController extends CrudController {
         $query = $this->em()->createQuery("SELECT e FROM \core\model\Usuario e WHERE UPPER(e.nome) LIKE :arg OR UPPER(e.login) LIKE :arg");
         $query->setParameter('arg', $arg);
         return $query->getResult();
+    }
+    
+    public function alterar_senha(SGAContext $context) {
+        $response = new AjaxResponse();
+        $id = (int) $context->getRequest()->getParameter('id');
+        $senha = $context->getRequest()->getParameter('senha');
+        $confirmacao = $context->getRequest()->getParameter('confirmacao');
+        $usuario = $this->findById($id);
+        if ($usuario) {
+            try {
+                $hash = $this->verificaSenha($senha, $confirmacao);
+                $query = $this->em()->createQuery("UPDATE \core\model\Usuario u SET u.senha = :senha");
+                $query->setParameter('senha', $hash);
+                $query->execute();
+                $response->success = true;
+            } catch (Exception $e) {
+                $response->message = $e->getMessage();
+            }
+        } else {
+            $response->message = _('Usuário inválido');
+        }
+        $context->getResponse()->jsonResponse($response);
     }
     
 }
