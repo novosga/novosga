0836a27a390651698b096689da57a6b3c7b17cf1
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2013-01-23 10:43:54 -0200

    Adicionado suporte ao cache do doctrine.

--- web/core/db/DB.php
+++ web/core/db/DB.php
@@ -9,6 +9,7 @@ use \core\Config;
 class DB {
     
     protected static $conn;
+    protected static $cacheDriver;
     protected static $em;
     
     public static function createConn($user, $pass, $host, $port, $dbname, $dbtype) {
@@ -39,10 +40,44 @@ class DB {
                 self::createConn(Config::DB_USER, Config::DB_PASS, Config::DB_HOST, Config::DB_PORT, Config::DB_NAME, Config::DB_TYPE);
             }
             $config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(self::$conn, Config::IS_DEV);
-            $config->setAutoGenerateProxyClasses(Config::IS_DEV);
+            $config->setAutoGenerateProxyClasses(true);
+            $dir = sys_get_temp_dir();
+            $config->setProxyDir($dir);
+            // caching
+            self::$cacheDriver = self::createDoctrineCache();
+            $config->setMetadataCacheImpl(self::$cacheDriver);
+            $config->setQueryCacheImpl(self::$cacheDriver);
+            $config->setResultCacheImpl(self::$cacheDriver);
             self::$em = \Doctrine\ORM\EntityManager::create(self::$conn, $config);
         }
         return self::$em;
+    }
+    
+    /**
+     * Retorna o cacheDriver do Doctrine de acordo com a configuração da aplicação (dev/prod)
+     * e com o driver disponível no servidor.
+     * 
+     * Quando IS_DEV é TRUE: 
+     *     retorna ArrayCache
+     * 
+     * Quando IS_DEV é FALSE: 
+     *     verifica, nessa ordem, se está disponível: APC e XCache.
+     *     caso nenhum esteja disponível, retorna o ArrayCache
+     * 
+     * @return \Doctrine\Common\Cache\ArrayCache
+     */
+    private static function createDoctrineCache() {
+        if (!Config::IS_DEV) {
+            // APC
+            if (extension_loaded('apc') && ini_get('apc.enabled')) {
+                return new \Doctrine\Common\Cache\ApcCache();
+            }
+            // XCache
+            else if (extension_loaded('xcache')) {
+                return new \Doctrine\Common\Cache\XcacheCache();
+            }
+        }
+        return new \Doctrine\Common\Cache\ArrayCache();
     }
     
 }
