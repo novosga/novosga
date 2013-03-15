8b0110a5cccc19986c953fcbf4af4c7eefae37d9
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2013-03-15 15:08:09 -0300

    Adicionada configuração da url do filme a ser exibido quando o painel está ocioso.

--- painel/client/src/org/novosga/painel/client/config/PainelConfig.java
+++ painel/client/src/org/novosga/painel/client/config/PainelConfig.java
@@ -1,5 +1,6 @@
 package org.novosga.painel.client.config;
 
+import java.io.File;
 import org.novosga.painel.config.ConfigParameter;
 import org.novosga.painel.config.AppConfig;
 import java.util.ArrayList;
@@ -16,8 +17,9 @@ public class PainelConfig extends AppConfig {
     public static final String KEY_UNIDADE = "UnidadeId";
     public static final String KEY_SERVICOS = "Servicos";
     public static final String KEY_SERVER = "IPServidor";
-    public static final String KEY_VIDEO_ID = "VideoID";
+    public static final String KEY_MONITOR_ID = "VideoID";
     public static final String KEY_SCREENSAVER_TIMEOUT = "ScreensaverTimeout";
+    public static final String KEY_SCREENSAVER_URL = "ScreensaverUrl";
     public static final String KEY_PROTOCOL = "Procolo";
     public static final String KEY_PORT_SEND = "PortaEnvio";
     public static final String KEY_PORT_RECEIVE = "PortaRecebimento";
@@ -35,8 +37,9 @@ public class PainelConfig extends AppConfig {
         parameters.add(new ConfigParameter<Integer>(KEY_UNIDADE, 0));
         parameters.add(new ConfigParameter<Integer[]>(KEY_SERVICOS, new Integer[]{0}));
         parameters.add(new ConfigParameter<String>(KEY_SERVER, ""));
-        parameters.add(new ConfigParameter<Integer>(KEY_VIDEO_ID, 0));
-        parameters.add(new ConfigParameter<Integer>(KEY_SCREENSAVER_TIMEOUT, 3));
+        parameters.add(new ConfigParameter<Integer>(KEY_MONITOR_ID, 0));
+        parameters.add(new ConfigParameter<Integer>(KEY_SCREENSAVER_TIMEOUT, 30));
+        parameters.add(new ConfigParameter<String>(KEY_SCREENSAVER_URL, new File("media/video/promo1.mp4").toURI().toString()));
         parameters.add(new ConfigParameter<String>(KEY_PROTOCOL, Main.DEFAULT_PROTOCOL));
         parameters.add(new ConfigParameter<Integer>(KEY_PORT_SEND, Main.DEFAULT_SEND_PORT));
         parameters.add(new ConfigParameter<Integer>(KEY_PORT_RECEIVE, Main.DEFAULT_RECEIVE_PORT));
--- painel/client/src/org/novosga/painel/config/AppConfig.java
+++ painel/client/src/org/novosga/painel/config/AppConfig.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import org.novosga.painel.client.Main;
