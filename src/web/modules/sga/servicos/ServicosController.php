6ef73bb236efb87ffdd0928e762156db00bec530
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2013-04-11 06:28:22 -0300

    Melhorada mensagens ao tentar excluir um serviço

--- src/web/modules/sga/servicos/ServicosController.php
+++ src/web/modules/sga/servicos/ServicosController.php
@@ -52,4 +52,35 @@ class ServicosController extends CrudController {
         $this->view()->assign('macros', $query->getResult());
     }
     
+    /**
+     * Verifica se já existe unidade usando o serviço.
+     * @param \core\model\SequencialModel $model
+     */
+    protected function preDelete(SGAContext $context, SequencialModel $model) {
+        $error = _('Já existem atendimentos para o serviço que está tentando remover');
+        // quantidade de atendimentos do servico
+        $query = $this->em()->createQuery("SELECT COUNT(e) as total FROM \core\model\Atendimento e JOIN e.servicoUnidade su WHERE su.servico = :servico");
+        $query->setParameter('servico', $model->getId());
+        $rs = $query->getSingleResult();
+        if ($rs['total'] > 0) {
+            throw new \Exception($error);
+        }
+        // quantidade de atendimentos do servico, no historico
+        $query = $this->em()->createQuery("SELECT COUNT(e) as total FROM \core\model\ViewAtendimento e WHERE e.servico = :servico");
+        $query->setParameter('servico', $model->getId());
+        $rs = $query->getSingleResult();
+        if ($rs['total'] > 0) {
+            throw new \Exception($error);
+        }
+        // apagando vinculo com as unidades
+        $this->em()->beginTransaction();
+        $query = $this->em()->createQuery("DELETE FROM \core\model\ServicoUnidade e WHERE e.servico = :servico");
+        $query->setParameter('servico', $model->getId());
+        $query->execute();
+    }
+    
+    protected function postDelete(SGAContext $context, SequencialModel $model) {
+        $this->em()->commit();
+    }
+    
 }
