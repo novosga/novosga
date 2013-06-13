780c25a814ab94fe08b2099e02724797bde432dc
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2013-06-13 16:54:58 -0300

    Atualizando estrutura html com base no boilerplate

--- src/web/core/view/SGAView.php
+++ src/web/core/view/SGAView.php
@@ -117,13 +117,21 @@ abstract class SGAView implements View {
         ob_start();
         ?>
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php SGA::out($title) ?></title>
+    <meta name="viewport" content="width=device-width">
     <?php $this->headerDependencies($context); ?>
 </head>
 <body class="<?php SGA::out($bodyClass) ?>">
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
     <div id="geral">
         <?php
         $html = ob_get_contents();
