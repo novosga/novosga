13d377122103001987200efaf6ba96d3eaea23ca
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2013-10-01 09:50:06 -0300

    acertado arquivo config.php

--- src/lib/novosga/Config.php
+++ src/lib/novosga/Config.php
@@ -3,11 +3,5 @@ namespace novosga;
 /* Arquivo de configuracao gerado automaticamente via ConfigWriter */
 class Config {
     const IS_DEV = true;
-    const SGA_INSTALLED = true;
-    const DB_TYPE = "pgsql";
-    const DB_HOST = "localhost";
-    const DB_PORT = "5432";
-    const DB_USER = "postgres";
-    const DB_PASS = "eco";
-    const DB_NAME = "basd";
+    const SGA_INSTALLED = false;
 }
