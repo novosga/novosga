0ef6eaf37483959ac9bad9da71fa1a11c694c6d5
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2013-10-11 22:31:32 -0300

    acertada dependencia composer

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 $autoload = __DIR__ . '/vendor/autoload.php';
 if (!file_exists($autoload)) {
-    echo '<h1>Novo SGA</h1><p>Please run <strong>composer.phar update</strong> fisrt</p>';
+    echo '<h1>Novo SGA</h1><p>Please run <strong>composer.phar install</strong> fisrt</p>';
     exit();
 }
 require $autoload;
