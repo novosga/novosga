146dd7759a57339584403e4af3b9373c2056feb2
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2014-03-12 21:57:15 -0300

    v1.1.3

--- src/Novosga/SGA.php
+++ src/Novosga/SGA.php
@@ -10,7 +10,7 @@ use \Novosga\Business\AcessoBusiness;
  */
 class SGA extends \Slim\Slim {
     
-    const VERSION = "1.1.2";
+    const VERSION = "1.1.3";
     const CHARSET = "utf-8";
     
     // SESSION KEYS
