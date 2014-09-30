2c830572590f4e0395731e0565a5e85fb8973313
Evaldo Barbosa <evaldobarbosa@gmail.com>
2014-09-30 00:27:31 -0300

    Issue de aumento do campo sigla_senha
    aumentando o tamanho de sigla para 3

--- src/Novosga/Model/ServicoUnidade.php
+++ src/Novosga/Model/ServicoUnidade.php
@@ -33,7 +33,7 @@ class ServicoUnidade extends Model {
     protected $local;
     /** @Column(type="string", name="nome", length=50, nullable=false) */
     protected $nome;
-    /** @Column(type="string", name="sigla", length=1, nullable=false) */
+    /** @Column(type="string", name="sigla", length=3, nullable=false) */
     protected $sigla;
     /** @Column(type="smallint", name="status", nullable=false) */
     protected $status;
