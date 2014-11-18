db50481be2755804887bcbb7d01aea58266a497c
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2014-11-18 15:29:17 -0200

    created model for oauth_client, issue #165

---
+++ src/Novosga/Model/OAuthClient.php
@@ -0,0 +1,25 @@
+<?php
+namespace Novosga\Model;
+
+/**
+ * 
+ * @Entity
+ * @Table(name="locais")
+ */
+class Local extends SequencialModel {
+
+    /** @Column(type="string", name="nome", length=20, nullable=false, unique=true) */
+    protected $nome;
+
+    public function __construct() {
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+}
