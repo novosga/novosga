c9f12be232d635bd11d893474037437737f7c2c2
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2014-11-24 18:49:59 -0200

    api atendimento view

--- src/Novosga/Api/ApiV1.php
+++ src/Novosga/Api/ApiV1.php
@@ -164,6 +164,21 @@ class ApiV1 extends Api {
     }
     
     /**
+     * Retorna o atendimento
+     * @param int $unidade
+     * @param int $usuario
+     * @return array
+     */
+    public function atendimento($id) {
+        // servicos que o usuario atende
+        $atendimento = $this->em->find('Novosga\Model\Atendimento', $id);
+        if (!$atendimento) {
+            throw new Exception(_('Atendimento inválido'));
+        }
+        return $atendimento->toArray(true);
+    }
+    
+    /**
      * Distribui uma nova senha
      */
     public function distribui($unidade, $usuario, $servico, $prioridade, $nomeCliente, $documentoCliente) {
