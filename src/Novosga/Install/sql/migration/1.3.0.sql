8e98a17ed6822f18cbe60f84d78ca2c54c4619e6
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2015-02-11 11:12:42 -0200

    fix atendimento_id on view_historico_atendimentos

--- src/Novosga/Install/sql/migration/1.3.0.sql
+++ src/Novosga/Install/sql/migration/1.3.0.sql
@@ -53,3 +53,52 @@ AS
         historico_atend_meta.value
     FROM 
         historico_atend_meta;
+
+
+DROP VIEW view_historico_atendimentos;
+
+CREATE VIEW view_historico_atendimentos 
+AS
+    SELECT 
+        atendimentos.id, 
+        atendimentos.unidade_id, 
+        atendimentos.usuario_id, 
+        atendimentos.usuario_tri_id, 
+        atendimentos.servico_id, 
+        atendimentos.prioridade_id, 
+        atendimentos.atendimento_id, 
+        atendimentos.status, 
+        atendimentos.sigla_senha, 
+        atendimentos.num_senha, 
+        atendimentos.num_senha_serv, 
+        atendimentos.nm_cli, 
+        atendimentos.num_local, 
+        atendimentos.dt_cheg, 
+        atendimentos.dt_cha, 
+        atendimentos.dt_ini, 
+        atendimentos.dt_fim, 
+        atendimentos.ident_cli 
+    FROM 
+        atendimentos 
+    UNION ALL 
+    SELECT 
+        historico_atendimentos.id, 
+        historico_atendimentos.unidade_id, 
+        historico_atendimentos.usuario_id, 
+        historico_atendimentos.usuario_tri_id, 
+        historico_atendimentos.servico_id, 
+        historico_atendimentos.prioridade_id, 
+        historico_atendimentos.atendimento_id,
+        historico_atendimentos.status, 
+        historico_atendimentos.sigla_senha, 
+        historico_atendimentos.num_senha, 
+        historico_atendimentos.num_senha_serv, 
+        historico_atendimentos.nm_cli, 
+        historico_atendimentos.num_local, 
+        historico_atendimentos.dt_cheg, 
+        historico_atendimentos.dt_cha, 
+        historico_atendimentos.dt_ini, 
+        historico_atendimentos.dt_fim, 
+        historico_atendimentos.ident_cli 
+    FROM 
+        historico_atendimentos;
