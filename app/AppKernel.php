d1d7a568b4976fc72234d87716fc05d85f2ee1a9
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2017-01-18 13:38:29 -0200

    api cors

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Mangati\BaseBundle\MangatiBaseBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
