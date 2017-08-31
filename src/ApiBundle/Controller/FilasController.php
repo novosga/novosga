ed77e41d0089cc5d1bb11b03d634e5e2d3b216b9
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2017-08-31 09:02:08 -0300

    filas api controller

---
+++ src/ApiBundle/Controller/FilasController.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Novo SGA project.
+ *
+ * (c) Rogerio Lino <rogeriolino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ApiBundle\Controller;
+
+use Novosga\Entity\Unidade;
+use Novosga\Entity\Usuario;
+use Novosga\Service\AtendimentoService;
+use Novosga\Service\FilaService;
+use Novosga\Service\UsuarioService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * FilasController
+ *
+ * @author Rogério Lino <rogeriolino@gmail.com>
+ * @Route("/filas")
+ */
+class FilasController extends Controller
+{
+    /**
+     * Retorna a lista de atendimentos do usuário atual na unidade informada.
+     * 
+     * @Route("/{unidadeId}")
+     * @ParamConverter("unidade", class="Novosga\Entity\Unidade", options={"id" = "unidadeId"})
+     * @Method("GET")
+     */
+    public function atendimentosUsuarioAction(Unidade $unidade)
+    {
+        /* @var $filaService FilaService */
+        $filaService    = $this->get('Novosga\Service\FilaService');
+        /* @var $usuarioService UsuarioService */
+        $usuarioService = $this->get('Novosga\Service\UsuarioService');
+        
+        $usuario      = $this->getUser();
+        $servicos     = $usuarioService->servicos($usuario, $unidade);
+        $atendimentos = $filaService->filaAtendimento($unidade, $servicos);
+        
+        return $this->json($atendimentos);
+    }
+    
+    /**
+     * Atualiza o statuso do atendimento atual do usuário para o novo status
+     * informado.
+     * 
+     * @Route("")
+     * @Method("PUT")
+     */
+    public function alteraStatusAction(Request $request)
+    {
+        $novoStatus = $request->get('novoStatus');
+        $usuario    = $this->getUser();
+        
+        /* @var $atendimentoService AtendimentoService */
+        $atendimentoService = $this->get('Novosga\Service\AtendimentoService');
+        $atendimento = $atendimentoService->alteraStatusAtendimentoUsuario($usuario, $novoStatus);
+        
+        return $this->json($atendimento);
+    }
+}
