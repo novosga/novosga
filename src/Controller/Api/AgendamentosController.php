47100a98594927bd32665a2747d8ce4058804bf1
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2017-11-24 11:11:39 -0200

    agendamento api #14

---
+++ src/Controller/Api/AgendamentosController.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the Novo SGA project.
+ *
+ * (c) Rogerio Lino <rogeriolino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller\Api;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * AgendamentosController
+ *
+ * @author Rogério Lino <rogeriolino@gmail.com>
+ *
+ * @Route("/api/agendamentos")
+ */
+class AgendamentosController extends ApiCrudController
+{
+    use Actions\GetTrait,
+        Actions\FindTrait;
+    
+    public function __construct()
+    {
+        parent::__construct(\Novosga\Entity\Agendamento::class);
+    }
+    
+    /**
+     * @Route("")
+     * @Method("POST")
+     */
+    public function postAction(
+        Request $request,
+        \Novosga\Service\AtendimentoService $atendimentoService
+    ) {
+        $json = $request->getContent();
+        $object = json_decode($json, true);
+        $agendamento = new \Novosga\Entity\Agendamento();
+        
+        $form = $this->createForm(\App\Form\Api\AgendamentoType::class, $agendamento, [
+
+        ]);
+        $form->submit($object);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            $message = 'Formulário inválido';
+            
+            foreach ($form->getErrors(true) as $error) {
+                $message . ': ' . $error->getMessage();
+                break;
+            }
+            
+            throw new \Exception($message);
+        }
+        
+        $clienteValido = $atendimentoService->getClienteValido($agendamento->getCliente());
+        
+        if (!$clienteValido) {
+            throw new \Exception('Favor preencher os dados do cliente.');
+        }
+        
+        $agendamento->setCliente($clienteValido);
+        
+        $atendimentoService->checkServicoUnidade($agendamento->getUnidade(), $agendamento->getServico());
+        
+        $om = $this->getDoctrine()->getManager();
+        $om->persist($agendamento);
+        $om->flush();
+        
+        return $this->json($agendamento);
+    }
+}
---
+++ src/Form/ClienteType.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Novo SGA project.
+ *
+ * (c) Rogerio Lino <rogeriolino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Form;
+
+use Novosga\Entity\Cliente;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Length;
+
+class ClienteType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nome', TextType::class, [
+                'constraints' => [
+                    new NotNull(),
+                    new Length([ 'min' => 3 ]),
+                ],
+            ])
+            ->add('documento', TextType::class, [
+                'constraints' => [
+                    new NotNull(),
+                    new Length([ 'min' => 3 ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+            ])
+            ->add('telefone', TextType::class, [
+            ])
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Cliente::class
+        ));
+    }
+}
