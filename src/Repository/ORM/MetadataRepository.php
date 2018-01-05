77abce66f5666c1e31acc5661ebc1ee87677c69e
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2018-01-05 09:43:19 -0200

    fixed metadata merge/persist

--- src/Repository/ORM/MetadataRepository.php
+++ src/Repository/ORM/MetadataRepository.php
@@ -43,14 +43,14 @@ class MetadataRepository extends EntityRepository implements MetadataRepositoryI
         
         if ($metada instanceof Metadata) {
             $metada->setValue($value);
-            $em->merge($entity);
+            $em->merge($metada);
         } else {
             $class  = $this->getEntityName();
             $metada = new $class;
             $metada->setEntity($entity);
             $metada->setName($name);
             $metada->setValue($value);
-            $em->persist($entity);
+            $em->persist($metada);
         }
         
         $em->flush();
