959684a88c308dfcfe6e9e07934dc9726f3a4226
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2018-02-28 19:08:21 -0300

    storage factory

---
+++ src/Infrastructure/StorageFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Infrastructure;
+
+use Exception;
+use App\Infrastructure\Storage\MySQLStorage;
+use Doctrine\Common\Persistence\ObjectManager;
+use Novosga\Infrastructure\StorageInterface;
+
+/**
+ * StorageFactory
+ *
+ * @author Rogerio Lino <rogeriolino@gmail.com>
+ */
+class StorageFactory
+{
+    public function createStorage(ObjectManager $om): StorageInterface
+    {
+        if ($om instanceof \Doctrine\ORM\EntityManager) {
+            $conn     = $om->getConnection();
+            $platform = $conn->getSchemaManager()->getDatabasePlatform();
+            
+            if ($platform instanceof \Doctrine\DBAL\Platforms\MySqlPlatform) {
+                return new MySQLStorage($om);
+            }
+            
+            if ($platform instanceof \Doctrine\DBAL\Platforms\PostgreSqlPlatform) {
+                // TODO: implement PostgreSQLStorage
+            }
+            
+            if ($platform instanceof \Doctrine\DBAL\Platforms\SQLServerPlatform) {
+                // TODO: implement SQLServerStorage
+            }
+        }
+        
+        throw new Exception('Novo SGA storage implemantation not found');
+    }
+}
