98833c36931367d24512d1471ae51f0b5659f8af
Rogerio Lino <rogeriolino@gmail.com>
2026-05-13 08:24:49 -0300

    fix: cliente data nascimento data type

--- src/Entity/Cliente.php
+++ src/Entity/Cliente.php
@@ -49,7 +49,7 @@ class Cliente implements ClienteInterface
     #[ORM\Column(length: 25, nullable: true)]
     private ?string $telefone = null;
 
-    #[ORM\Column(name: 'dt_nascimento', type: Types::DATETIME_MUTABLE, nullable: true)]
+    #[ORM\Column(name: 'dt_nascimento', type: Types::DATE_MUTABLE, nullable: true)]
     private ?DateTimeInterface $dataNascimento = null;
 
     #[ORM\Column(length: 1, nullable: true)]
